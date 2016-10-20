f365c158915a334b378ca935a3ef6b4e58c37bce
biancabouleanu <biancabouleanu@gmail.com>
2016-10-20 17:02:49 +0300

    Add files via upload

---
+++ PSSC/Models/Nota.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    enum TipNota
+    {
+        notaExamen,
+        notaDistribuita
+    }
+    class Nota
+    {
+      
+        Discipline disciplina;
+        Student student;
+        TipNota tNota;
+
+        public Discipline Disciplina
+        {
+            get { return disciplina; }
+            set { disciplina = value; }
+        }
+        public TipNota tnota
+        {
+            get { return tNota; }
+            set { tNota = value; }
+        }
+        public Student Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+        public Nota(Discipline disciplina, Student student, TipNota tNota)
+        {
+            this.disciplina = disciplina;
+            this.Student = student;
+            this.tNota = tNota;
+        }
+    }
+}
---
+++ PSSC/Models/Profesor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Profesor
+    {
+        string nume;
+        string prenume;
+        string telefon;
+        List<Discipline> discipline = new List<Discipline>();
+        List<Nota> note = new List<Nota>();
+
+        public string Nume 
+        {
+            get { return nume; }
+            set { nume = value; }
+        }
+        public string Prenume
+        {
+            get { return prenume; }
+            set { prenume = value; }
+        }
+        public string Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+        private List<Discipline> Disciplina
+        {
+            get { return discipline; }
+            set { discipline = value; }
+        }
+        private List<Nota> Nota
+        {
+            get { return note; }
+            set { note = value; }
+        }
+        public Profesor(string nume, string prenume, string telefon)
+        {
+            this.nume = nume;
+            this.prenume = prenume;
+            this.telefon = telefon;
+        }
+    }
+}
